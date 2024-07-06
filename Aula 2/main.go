116b56a82c5d935a38ea09288ed7b72abb8908a3
GuilhermeSSantos2004 <guilhermesilvas2004@gmail.com>
2024-07-06 13:23:27 -0300

    fix bug

--- Aula 2/main.go
+++ Aula 2/main.go
@@ -1,7 +1,23 @@
 package main
 
-var b bool
+// declaração de variáveis GLOBAIS, você pode acessar em todas as funções dentro do arquivos / pagote
+// variáveis de escopo GLOBAIS
+var (
+	a string
+	b bool
+	c int
+	d float64
+)
 
 func main() {
-	println(b)
+
+	var e string = "Guilherme" // Declaração e atruibução no exato momento
+	var f string               // variável de escopo local
+
+	println(a) // imprimi em branco (nada) por padrão
+	println(b) // imprimi um valor bool (FALSE) por padrão
+	println(c) // imprimi um valor int (0) por padrão
+	println(d) // imprimi um valor float64 ou float32 (0.000000.000) por padrão
+	println(e) // imprimi a Declaração e atruibução de uma variavel que foi declarada no exato momento
+	println(f) // imprimi um valor de uma variável de escopo local
 }
