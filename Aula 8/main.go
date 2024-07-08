302000ebc251e428f85d867c80c2bd7c7cfd1d23
guilherme <guilhermesilvas2004@gmail.com>
2024-07-07 21:43:11 -0300

    funcao dois arg

--- Aula 8/main.go
+++ Aula 8/main.go
@@ -12,12 +12,12 @@ func main() {
 	}
 	fmt.Println(valor)
 
-	sub, codicao := sub(0,1)
+	sub, codicao := sub(0, 1)
 
-	fmt.Printf("O valor retornado é %d a condição é",sub , codicao)
+	fmt.Printf("O valor retornado é %d a condição é %t", sub, codicao)
 }
 
-func sum(a, b int) (int, error) {   //nome_func(var_receber <type>,var_receber <type>) (<type>, <type>) => Esse segundo parentes é para os tipos dos valores que serão retornados
+func sum(a, b int) (int, error) { //nome_func(var_receber <type>,var_receber <type>) (<type>, <type>) => Esse segundo parentes é para os tipos dos valores que serão retornados
 	if a+b >= 50 {
 		return 0, errors.New("A soma é maior que 50")
 	}
@@ -26,8 +26,8 @@ func sum(a, b int) (int, error) {   //nome_func(var_receber <type>,var_receber <
 
 func sub(a, b int) (int, bool) {
 
-	if a - b <= 0 {
-		return a - b , false 
+	if a-b <= 0 {
+		return a - b, false
 	}
-	return a - b , true
+	return a - b, true
 }
