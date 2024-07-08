52f2f9706f02f7b8b3e8351cfe8322147e9b1ebb
guilherme <guilhermesilvas2004@gmail.com>
2024-07-07 23:19:33 -0300

    Closures

---
+++ Aula 10/main.go
@@ -0,0 +1,21 @@
+package main
+
+//Closures
+
+import "fmt"
+
+func main() {
+
+	totalvezesdobro := func() int {
+		return sum(1, 3, 45, 6, 34, 654, 654, 7645, 534, 543, 543, 543) * 2
+	}()
+	fmt.Println(totalvezesdobro)
+}
+
+func sum(numeros ...int) int {
+	total := 0
+	for _, numero := range numeros {
+		total += numero
+	}
+	return total
+}
