b64f6e06aac5f00f0804dcc312d7e1a6760df062
guilherme <guilhermesilvas2004@gmail.com>
2024-08-04 17:36:59 -0300

    Criando Array

--- Aula 5/main.go
+++ Aula 5/main.go
@@ -4,12 +4,24 @@ import "fmt"
 
 func main() {
 	var meuArray [3]int
+	var seuArray [6]int
 
 	meuArray[0] = 1
 	meuArray[1] = 2
 	meuArray[2] = 3
 
+	seuArray[0] = 1
+	seuArray[1] = 2
+	seuArray[2] = 3
+	seuArray[3] = 4
+	seuArray[4] = 5
+	seuArray[5] = 6
+
 	for i, v := range meuArray {
+		fmt.Printf("O valor do indice é %d e o valor que está localizado no indice é: %d\n", i, v)
+	}
+
+	for i, v := range seuArray {
 		fmt.Printf("O valor do indice é %d e o valor que está localizado no indice é: %d\n", i, v)
 	}
 }
