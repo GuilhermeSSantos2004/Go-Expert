300329cf6c14d848928dca116d36277c5b41d360
guilherme <guilhermesilvas2004@gmail.com>
2025-03-16 21:22:34 -0300

    func variadicas

--- Aula-9/main.go
+++ Aula-9/main.go
@@ -8,7 +8,7 @@ func main() {
 	fmt.Println(sum(1, 3, 45, 6, 34, 654, 654, 7645, 534, 543, 543, 543))
 }
 
-func sum(numeros ...int) int {
+func sum(numeros ...int) int { 	// O uso de '...int' permite passar um número variável de argumentos do tipo int
 	total := 0
 	for _, numero := range numeros {
 		total += numero
