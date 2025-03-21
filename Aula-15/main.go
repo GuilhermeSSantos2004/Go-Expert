ee7c463295a87e01baf028fcea98755843278787
guilherme <guilhermesilvas2004@gmail.com>
2025-03-21 07:05:22 -0300

    Interfaces vazias

--- Aula-15/main.go
+++ Aula-15/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var x interface{} = 10 
+	var y interface{} = "Hello World" 
+
+	showType(x)
+	showType(y)
+}
+func showType(t interface{}) {
+	fmt.Printf("O tipo da variável é %T e o valor é %v\n", t, t)
 }
