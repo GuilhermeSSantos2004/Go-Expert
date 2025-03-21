98cbc0faea33639166957a7339e9da645893edb0
guilherme <guilhermesilvas2004@gmail.com>
2025-03-21 06:54:20 -0300

    Ponteiros e Structs

--- Aula-14/main.go
+++ Aula-14/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import "fmt"
 
+type Cliente struct {
+	nome string
+}
+
+type Conta struct {
+	saldo int 
+}
+
+func newConta() *Conta { //
+	return &Conta{saldo: 0}
+}
+
+func (c Cliente) andou() {
+	c.nome = "Wesley Willians"
+	fmt.Printf("O cliente  %v andou\n", c.nome)
+}
+
+// *Conta = Referência (ponteiro) e Afeta o valor original != Conta = Cópia e não Afeta original
+func (c *Conta) simular(valor int) int {
+	c.saldo += valor
+	println(c.saldo)
+	return c.saldo 
+}
+
+func main() {
+	Wesley := Cliente{
+		nome: "Wesley",
+	}
+	Wesley.andou()
+	fmt.Printf("O valor da struct com nome %v", Wesley.nome)
+
+	conta := Conta{saldo: 100}
+	conta.simular(200)
+	println(conta.saldo)
 }
