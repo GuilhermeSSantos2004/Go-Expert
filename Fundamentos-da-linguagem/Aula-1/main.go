3ae867af3d85708a2630b2811e279590176a42c7
guilherme <guilhermesilvas2004@gmail.com>
2025-03-23 11:55:38 -0300

    feat: entendendo a primira linha

--- Fundamentos-da-linguagem/Aula-1/main.go
+++ Fundamentos-da-linguagem/Aula-1/main.go
@@ -1,12 +1,18 @@
-package main 
+package main // package main é obrigatório para criar um programa executável
 
-string firstcode = "Hello World"
+// Quando você usa package main, está dizendo ao compilador:
+// "Este é o ponto de entrada do meu programa."
 
-func main(){
-	//O nome do pacote deve ser o nome da pasta onde está o arquivo.
-	//Erros : Não pode ter dois pagotes dentro da mesma pasta
-	// Curiosidade: As variáveis que são declaradas no pagote A podem 
-	// ser vistas como variáveis globais em todos os arquivos que estiverem declarados o pacote XA mas não no pagote XB 
+// O Go vai procurar uma função main() dentro do package main.
+// É essa função que será executada quando você rodar o programa.
+
+func main() {
+	// O nome do pacote deve ser o nome da pasta onde está o arquivo.
+	// Erros: Não pode ter dois pacotes dentro da mesma pasta
+	// Curiosidade: As variáveis que são declaradas no pacote A podem 
+	// ser vistas como variáveis globais em todos os arquivos que estiverem declarados no pacote A, 
+	// mas não no pacote B.
+
+	firstcode := "Hello World"
 	print(firstcode)
 }
-
